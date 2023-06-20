5972375fc227666bde141dc3a8042e4ea1e3f3e4
Frikimatii <matibiencomodo@gmail.com>
2023-06-20 17:12:47 -0300

    Update1.1

---
+++ js.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.headline'); 
+ScrollReveal().reveal('.tagline', { delay: 1000 });
+ScrollReveal().reveal('.taglin', { delay: 500 });
+ScrollReveal().reveal('.bo1', { delay: 1000 });
+ScrollReveal().reveal('.bo2', { delay: 1500 });
