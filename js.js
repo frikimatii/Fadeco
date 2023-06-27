4f772a9ceee932241f41dc0a7469968d895198b1
Frikimatii <matibiencomodo@gmail.com>
2023-06-26 21:01:47 -0300

    coreccion de ortografia / animacion

--- js.js
+++ js.js
@@ -1,5 +1,4 @@
 ScrollReveal().reveal('.headline'); 
 ScrollReveal().reveal('.tagline', { delay: 1000 });
 ScrollReveal().reveal('.taglin', { delay: 500 });
-ScrollReveal().reveal('.bo1', { delay: 1000 });
-ScrollReveal().reveal('.bo2', { delay: 1500 });
+ScrollReveal().reveal('.bo1', { delay: 500 });
